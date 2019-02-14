afedc658d531379e5a6997629f9a53e7fa79bde8
Tim <tim7304@gmail.com>
2019-02-14 16:53:13 +0100

    deleted main.m --> replaced by photometric_stereo.m

--- photometric/photometric_stereo.m
+++ photometric/photometric_stereo.m
@@ -6,7 +6,8 @@ disp('Part 1: Photometric Stereo')
 
 % obtain many images in a fixed view under different illumination
 disp('Loading images...')
-image_dir = './SphereGray5/';   % TODO: get the path of the script
+% image_dir = './SphereGray5/';   % TODO: get the path of the script
+image_dir = strcat(pwd,'/photometrics_images/SphereGray5');
 %image_ext = '*.png';
 
 [image_stack, scriptV] = load_syn_images(image_dir);
