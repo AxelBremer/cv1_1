095b6dd0543e93da97bdd95f21f6a8a2dd0e2462
Tim <tim7304@gmail.com>
2019-02-14 13:44:33 +0100

    added mac/linux compatibility for read_images.m

--- photometric/read_images.m
+++ photometric/read_images.m
@@ -1,13 +1,28 @@
-folder = strcat(pwd,'\photometrics_images\SphereGray5');
+if ismac || isunix
+    folder = strcat(pwd,'/photometrics_images/SphereGray5');
+elseif ispc
+    folder = strcat(pwd,'\photometrics_images\SphereGray5');
+else
+    disp('Platform not supported')
+end
 
-filePattern = fullfile(folder, '*.png');
-pngFiles = dir(filePattern)
-disp(pngFiles(1))
-for k = 1:length(pngFiles)
-    baseFileName = pngFiles(k).name;
-    fullFileName = fullfile(folder, baseFileName);
-    %fprintf(1, 'Now reading %s\n', fullFileName);
-    imageArray = imread(fullFileName);
-    imshow(imageArray);  % Display image.
-    %drawnow; % Force display to update immediately.
+filePattern = fullfile(folder, '*.png');
+
+pngFiles = dir(filePattern);
+
+disp(pngFiles(1))
+
+
+for k = 1:length(pngFiles)
+    baseFileName = pngFiles(k).name;
+
+    fullFileName = fullfile(folder, baseFileName);
+
+    %fprintf(1, 'Now reading %s\n', fullFileName);
+    
+    imageArray = imread(fullFileName);
+
+    imshow(imageArray);  % Display image.
+
+    %drawnow; % Force display to update immediately.
 end
