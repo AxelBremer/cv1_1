951ecbba86b3cd64706855c99abe91ea0df695c3
Ruben <rubenvanerkelens@hotmail.com>
2019-02-19 16:03:27 +0100

    3: image formation

--- intrinsic_images/iid_image_formation.m
+++ intrinsic_images/iid_image_formation.m
@@ -1,5 +1,9 @@
 
-albedo = imread('ball_albedo.png');
-shading = imread('ball_shading.png');
-image = albedo.*shading;
-imshow(image);
+albedo = double(imread('ball_albedo.png'));
+shading = double(imread('ball_shading.png'));
+ball = imread('ball.png');
+image = ( shading/mean(shading,'all') ).*( albedo/ (mean(albedo,'all')^2) );
+figure, imshow(image);
+title('constructed image');
+figure, imshow(ball);
+title('original image')
